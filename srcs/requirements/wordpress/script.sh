6e04de2387907af4339e6faf3d5f66f7274b8582
ayoub bouassid <abouassi@abouassi42.abouassi>
2023-11-25 03:13:43 -0600

    ho

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,16 +1,17 @@
 #!bin/bash
 
+cd /var/www/wordpress
 #Downloading and extracting Wordpress core files to the current directory
 until mariadb -h mariadb -u abouassi -p1234 -e ";"; do
-    echo "sber azbi...."
+    echo "sber ...."
     sleep 2
 done
-echo "nadi azamel...."
+echo "nadi ...."
 wp core download --allow-root
 # Creating the wp-config.php file using this command.
 wp config create	--allow-root --dbname=abouassidb --dbuser=abouassi --dbpass=1234 --dbhost=mariadb --path='/var/www/wordpress'
 # Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
-wp core install --url=${DOMAIN_NAME} --title="DopamInception" --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --path='/var/www/wordpress'
+wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
 # exec $@cd /var/www/html/wordpress
 # #Downloading and extracting Wordpress core files to the current directory
 # wp core download --allow-root
