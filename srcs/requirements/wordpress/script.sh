4661ce887b2adb9e4dff4f85d67ae84eb3f54bfd
ayoub bouassid <abouassi@abouassi42.abouassi>
2023-11-25 04:59:14 -0600

    HH

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -11,13 +11,5 @@ wp core download --allow-root
 # Creating the wp-config.php file using this command.
 wp config create	--allow-root --dbname=abouassidb --dbuser=abouassi --dbpass=1234 --dbhost=mariadb --path='/var/www/wordpress'
 # Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
-wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
-# exec $@cd /var/www/html/wordpress
-# #Downloading and extracting Wordpress core files to the current directory
-# wp core download --allow-root
-# # Creating the wp-config.php file using this command.
-# wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
-# # Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
-# wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --a
-
+wp core install --url=${DOMAIN_NAME} --title=AYOUBinception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
 php-fpm7.3 -F
